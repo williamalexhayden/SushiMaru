f3c010fa3ce4900be6b0a681cf9bef6f89e0e1de
williamalexhayden <williamalexhayden@yahoo.com>
2015-03-20 01:15:21 -0700

    sidebar functioning style almost complete

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(".launch.button").mouseenter(function(){
+        $(this).stop().animate(function(){$(this).find('.text').show();});
+    }).mouseleave(function (event){
+        $(this).find('.text').hide();
+        $(this).stop().animate();
+    });
+$("#nav").sidebar({overlay: true})
+    .sidebar('attach events','.ui.launch.button');
+    
